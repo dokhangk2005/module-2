24683c166cba06735bc20c55604f1c11ccc09507
dokhangk2005 <dokhangk2005@gmail.com>
2024-09-21 21:05:41 +0700

    nop bai

---
+++ module2/src/ss6/bai_tap/PointAndMoveablePoint/MoveablePoint.java
@@ -0,0 +1,56 @@
+package ss6.bai_tap.PointAndMoveablePoint;
+
+public class MoveablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MoveablePoint() {
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", speed=(" + xSpeed + "," + ySpeed + ")";
+    }
+
+    public MoveablePoint move() {
+        setX(super.getX() + xSpeed);
+        setY(super.getY() + ySpeed);
+        return this;
+    }
+}
