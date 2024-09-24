ecdf9f8926fac89ee5750c8fca6e85721087540a
dokhangk2005 <dokhangk2005@gmail.com>
2024-09-24 20:58:27 +0700

    nop bai

--- module2/src/ss3/bai_tap/minArr.java
+++ module2/src/ss3/bai_tap/minArr.java
@@ -2,26 +2,26 @@ package ss3.bai_tap;
 
 import java.util.Scanner;
 
-public class maxArr {
-    public static void maxArray(int arr[][]) {
+public class minArr {
+    public static void minArray(int arr[][]) {
         int arr2[] = new int[arr.length];
-        int maxArr2 = arr2[0];
+        int minArr2 = arr2[0];
         for (int i = 0; i < arr.length; i++) {
-            int maxRow = arr[i][0];
+            int minRow = arr[i][0];
             for (int j = 0; j < arr[0].length; j++) {
-                if (maxRow < arr[i][j]) {
-                    maxRow = arr[i][j];
+                if (minRow > arr[i][j]) {
+                    minRow = arr[i][j];
                 }
             }
-            arr2[i] = maxRow;
+            arr2[i] = minRow;
         }
 
         for (int i = 0; i < arr2.length; i++) {
-            if (maxArr2 < arr2[i]) {
-                maxArr2 = arr2[i];
+            if (minArr2 < arr2[i]) {
+                minArr2 = arr2[i];
             }
         }
-        System.out.println("So lon nhat trong mang 2 chieu la " + maxArr2);
+        System.out.println("So be nhat trong mang 2 chieu la " + minArr2);
     }
 
     public static void main(String[] args) {
@@ -30,7 +30,7 @@ public class maxArr {
         int r = sc.nextInt();
         System.out.print("Nhập số cột: ");
         int c = sc.nextInt();
-        int arr[][] = {{r}, {c}};
+        int arr[][] = {{r},{c}};
         System.out.println("Nhập các phần tử của mảng: ");
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
@@ -38,6 +38,6 @@ public class maxArr {
                 arr[i][j] = sc.nextInt();
             }
         }
-        maxArray(arr);
+        minArray(arr);
     }
 }
